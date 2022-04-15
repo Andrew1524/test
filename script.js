65e0ec7d03c3741713776e9aef3c768de3f203c5
Andrey <abassketball15@gmail.com>
2022-04-16 01:01:31 +0300

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,80 @@
+//* "coment me" коментарий однострочный
+/* "And"
+		"ME"*/ //*коментарий многострочный
+
+//*alert("Hello world!");
+
+//*console.log("Hello world!");
+
+/*var myFirstVar = 50;
+
+myFirstVar = 25;
+
+console.log(myFirstVar)*/
+
+/*console.log(4+6);
+console.log(4+ "I am string");
+console.log("Hello" + " World");*/
+
+/*var incr = 10,
+	decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);*/
+
+//*console.log(10%3);
+
+/*var num = "5";
+console.log(num != 5);
+console.log(num === 5);*/
+
+/*var number = 5;
+var string = "Hello world";
+var symbol;
+true;
+false;
+
+console.log(6/2 == 3);
+console.log(10/5 == 3);
+
+var und;
+
+console.log(und);
+
+var obj = {};
+
+console.log(typeof obj);
+console.log(typeof string);*/
+
+//Циклы и условные операторы!!!
+/*
+if (10 + 5 == 15) {
+	alert("It's a true!");
+}
+else if (10 + 5 == 15) {
+	alert("Wrong!");
+}
+*/
+
+//Цикл "for"
+
+/*
+for (var i = 1; i < 8; i++) {
+	console.log(i);
+};
+*/
+
+//function
+
+var out = 5;
+
+function myFirstFunc(x, y) {
+	var inner = 10;
+
+	return (x+y);
+}
+console.log(myFirstFunc(5,5));
+//console.log(inner);
